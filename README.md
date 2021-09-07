e62b9e64ca241b89eb3a153a48eed3e3ce47bbef
Neha Bharti <nehabharti@Nehas-MacBook-Air.local>
2021-09-07 12:59:44 +0530

    fixed icon size

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-Instructions
+#Instructions
 
 1. Clone this repository and install the dependencies using yarn.
 2. This repository is based on create-react-app, you can use yarn start to run the application.
+
--- src/components/PlusIcon.js
+++ src/components/PlusIcon.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 import { MdAdd } from "react-icons/md";
 
 const PlusIcon = styled(MdAdd)`
-width: 0.75rem;
-height: 0.75rem;
+width: 1rem;
+height: 1rem;
 margin-right: 20px;
 margin-left: -4px;
-margin-top: 4px;
+margin-top: 2px;
 `;
 
 PlusIcon.displayName = "PlusIcon";
